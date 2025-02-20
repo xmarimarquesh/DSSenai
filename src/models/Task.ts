eea3f19e13331fc6e832fafbde9e92395578f18a
xmarimarquesh <marimarquesh39@gmail.com>
2025-02-20 15:43:39 -0300

    ok

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -3,14 +3,14 @@ import mongoose, { Schema, Document } from 'mongoose';
 interface ITask extends Document {
     title: string;
     description: string;
-    data: Date;
+    date: Date;
     status: string;
 }
 
 const taskSchema: Schema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
-    data: { type: String, required: true },
+    date: { type: String, required: true },
     status: { type: String, required: true },
 });
 
